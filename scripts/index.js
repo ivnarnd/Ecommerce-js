cd3bb21f20ebc61b892aad82de66520a10ccbdf7
Ivan Aranda <iaranda264@gmail.com>
2023-07-10 16:44:48 -0300

    corrigiendo el stock de los sabores

--- scripts/index.js
+++ scripts/index.js
@@ -10,52 +10,52 @@ const sabores = [
     {
       nombre: "Dulce de leche",
       descripcion: "Delicioso y cremoso helado con el sabor inconfundible del dulce de leche argentino, perfecto para los amantes de lo dulce y caramelizado.",
-      stock: 10
+      stock: 10.000
     },
     {
       nombre: "Frutilla",
       descripcion: "Refrescante y vibrante, este helado de frutilla ofrece el equilibrio perfecto entre dulzura y acidez, resaltando el sabor natural y jugoso de las fresas.",
-      stock: 8
+      stock: 8.000
     },
      {
       nombre: "Chocolate amargo",
       descripcion: "Para los amantes del chocolate intenso, este helado ofrece una experiencia rica y profunda, con un sabor a cacao puro que deleitará a los paladares más exigentes.",
-      stock: 5
+      stock: 5.000
       },
     {
       nombre: "Menta granizada",
       descripcion: "Una combinación refrescante y crujiente, este helado de menta se mezcla con pequeños trozos de chocolate para crear una experiencia deliciosa y revitalizante.",
-      stock: 12
+      stock: 12.000
      },
     {
       nombre: "Banana split",
       descripcion: "Inspirado en el famoso postre, este helado combina el sabor suave y cremoso del helado de banana con una variedad de ingredientes adicionales, como salsa de chocolate, nueces y crema batida, para una experiencia indulgente.",
-      stock: 6
+      stock: 6.000
     },
     {
       nombre: "Sambayón",
       descripcion: "El helado de sambayón ofrece una combinación única de yemas de huevo y vino, creando un sabor suave y dulce con un toque de sofisticación.",
-      stock: 3
+      stock: 3.000
     },
      {
       nombre: "Crema americana",
       descripcion: "Este helado de vainilla cremoso es enriquecido con trozos generosos de dulce, agregando una textura y sabor extra a cada cucharada.",
-      stock: 7
+      stock: 7.000
      },
     {
       nombre: "Frutos del bosque",
       descripcion: "Una explosión de sabores naturales, este helado combina una mezcla de frutas del bosque, como frambuesas, moras y arándanos, para crear una experiencia frutal y vibrante.",
-      stock: 9
+      stock: 9.000
     },
     {
       nombre: "Maracuyá",
       descripcion: "Este helado tropical de maracuyá ofrece un sabor agridulce y refrescante, capturando la esencia de la exótica fruta de la pasión.",
-      stock: 4
+      stock: 4.000
     },
      {
       nombre: "Coco rallado",
       descripcion: "Un sabor tropical y cremoso, este helado de coco está lleno de trozos suaves de coco rallado, brindando una textura suave y un sabor tropical irresistible.",
-      stock: 11}
+      stock: 11.000}
     ];
   
 const escogerSabores=(cantidad)=>{
