f84b4da9d65238433a5133b3bfbe8a4f36d009c0
Ivan Aranda <iaranda264@gmail.com>
2023-07-10 14:19:12 -0300

    Agregando los sabores de helados

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,56 @@
 /*codigo de JavaScript*/
+const sabores = [
+    {
+      nombre: "Dulce de leche",
+      descripcion: "Delicioso y cremoso helado con el sabor inconfundible del dulce de leche argentino, perfecto para los amantes de lo dulce y caramelizado.",
+      stock: 10
+    },
+    {
+      nombre: "Frutilla",
+      descripcion: "Refrescante y vibrante, este helado de frutilla ofrece el equilibrio perfecto entre dulzura y acidez, resaltando el sabor natural y jugoso de las fresas.",
+      stock: 8
+    },
+     {
+      nombre: "Chocolate amargo",
+      descripcion: "Para los amantes del chocolate intenso, este helado ofrece una experiencia rica y profunda, con un sabor a cacao puro que deleitará a los paladares más exigentes.",
+      stock: 5
+      },
+    {
+      nombre: "Menta granizada",
+      descripcion: "Una combinación refrescante y crujiente, este helado de menta se mezcla con pequeños trozos de chocolate para crear una experiencia deliciosa y revitalizante.",
+      stock: 12
+     },
+    {
+      nombre: "Banana split",
+      descripcion: "Inspirado en el famoso postre, este helado combina el sabor suave y cremoso del helado de banana con una variedad de ingredientes adicionales, como salsa de chocolate, nueces y crema batida, para una experiencia indulgente.",
+      stock: 6
+    },
+    {
+      nombre: "Sambayón",
+      descripcion: "El helado de sambayón ofrece una combinación única de yemas de huevo y vino, creando un sabor suave y dulce con un toque de sofisticación.",
+      stock: 3
+    },
+     {
+      nombre: "Crema americana",
+      descripcion: "Este helado de vainilla cremoso es enriquecido con trozos generosos de dulce, agregando una textura y sabor extra a cada cucharada.",
+      stock: 7
+     },
+    {
+      nombre: "Frutos del bosque",
+      descripcion: "Una explosión de sabores naturales, este helado combina una mezcla de frutas del bosque, como frambuesas, moras y arándanos, para crear una experiencia frutal y vibrante.",
+      stock: 9
+    },
+    {
+      nombre: "Maracuyá",
+      descripcion: "Este helado tropical de maracuyá ofrece un sabor agridulce y refrescante, capturando la esencia de la exótica fruta de la pasión.",
+      stock: 4
+    },
+     {
+      nombre: "Coco rallado",
+      descripcion: "Un sabor tropical y cremoso, este helado de coco está lleno de trozos suaves de coco rallado, brindando una textura suave y un sabor tropical irresistible.",
+      stock: 11}
+    ];
+  
 const escogerSabores=(cantidad)=>{
     let cartel = 'Usted escogio los Siguientes sabores: ';
     while(cantidad > 0){
