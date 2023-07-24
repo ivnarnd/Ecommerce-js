a8dcdf6dd16fc8bd9762ea3a33b5f92fbbf7c3e5
Ivan Aranda <iaranda264@gmail.com>
2023-07-24 15:16:14 -0300

    comparando datos con local storage

--- scripts/index.js
+++ scripts/index.js
@@ -194,17 +194,22 @@ const iniciar = (e) => {
     const apellido = e.target.children[3].value;
     const fechaNac = new Date(e.target.children[5].value);
     const edad = Math.floor((fechaActual - fechaNac) / (1000 * 60 * 60 * 24 * 365.25));
-
+    let usuario;
+    
     if (edad >= 18) {
-        let usuario = new Usuario(nombre,apellido,edad,[],[]);
-        localStorage.setItem('usuario',JSON.stringify(usuario));
-        window.location.href='./pages/tienda.html';
-        
-
-        
-    } else {
+        const usuarioenLS=JSON.parse(localStorage.getItem('usuario'));
+        if(usuarioenLS.nombre == nombre && usuarioenLS.apellido == apellido){
+            usuario = usuarioenLS;
+            window.location.href='./pages/tienda.html';
+        }else{
+            usuario = new Usuario(nombre,apellido,edad,[],[]);
+            localStorage.setItem('usuario',JSON.stringify(usuario));
+            window.location.href='./pages/tienda.html';
+        }
+     } else {
         alert("Eres menor de edad. Debes tener al menos 18 años para acceder.");
     }
 };
+localStorage.setItem('Sabores',JSON.stringify(sabores));
 let form = document.getElementById('formulario_inicio');
 form.addEventListener('submit',iniciar);
