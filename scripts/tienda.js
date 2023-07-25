a88b87becf65eb72df243a911379468d745b75ed
Ivan Aranda <iaranda264@gmail.com>
2023-07-24 23:07:47 -0300

    mostrando solo sabores disponibles

--- scripts/tienda.js
+++ scripts/tienda.js
@@ -1,10 +1,10 @@
 let sabores = JSON.parse(localStorage.getItem('Sabores'));
 let usuario = JSON.parse(localStorage.getItem('usuario'));
-
+let saboresDisp = sabores.filter((sab)=> sab.stock>=250);
 let {nombre,apellido,edad,compras,favoritos} = usuario;
 
 let contenedorSabores=document.getElementById('contenedor-sabores');
-for (const sabor of sabores) {
+for (const sabor of saboresDisp) {
     let contenedor = document.createElement('div');
     contenedor.classList.add('sabor-item');
     contenedor.innerHTML=
@@ -21,96 +21,96 @@ for (const sabor of sabores) {
 }
 
 
-const escogerSabores=(cantidad,tipo)=>{
-    let saboresElegidos = [];
-    let cartelSabores = `Escoja un Sabor de helado: \n`;
-    let gr = 0;
-    let sabor = -1;
-    switch (tipo) {
-        case 1:
-            gr = 1000 / cantidad;
-            break;
-        case 2:
-                gr = 500 / cantidad;
-                break;
-        case 3: gr = 250 / cantidad;
-                break;
-        default:
-            gr = 0;
-            break;
-    }
+// const escogerSabores=(cantidad,tipo)=>{
+//     let saboresElegidos = [];
+//     let cartelSabores = `Escoja un Sabor de helado: \n`;
+//     let gr = 0;
+//     let sabor = -1;
+//     switch (tipo) {
+//         case 1:
+//             gr = 1000 / cantidad;
+//             break;
+//         case 2:
+//                 gr = 500 / cantidad;
+//                 break;
+//         case 3: gr = 250 / cantidad;
+//                 break;
+//         default:
+//             gr = 0;
+//             break;
+//     }
 
 
-    let saboresDisp = sabores.filter(sabor => sabor.stock >= gr );
-    for (let i = 0; i < saboresDisp.length; i++) {
-        cartelSabores = cartelSabores +`${i} ${saboresDisp[i].nombre}\n`;    
-    }
-    while(cantidad > 0){
+//     let saboresDisp = sabores.filter(sabor => sabor.stock >= gr );
+//     for (let i = 0; i < saboresDisp.length; i++) {
+//         cartelSabores = cartelSabores +`${i} ${saboresDisp[i].nombre}\n`;    
+//     }
+//     while(cantidad > 0){
        
-        sabor = parseInt(prompt(cartelSabores));
-        saboresElegidos.push(saboresDisp[sabor].nombre);
-        saboresDisp[sabor].stock = saboresDisp[sabor].stock - gr;
-        cantidad--; 
-    }
+//         sabor = parseInt(prompt(cartelSabores));
+//         saboresElegidos.push(saboresDisp[sabor].nombre);
+//         saboresDisp[sabor].stock = saboresDisp[sabor].stock - gr;
+//         cantidad--; 
+//     }
         
     
-    return saboresElegidos;
-}
+//     return saboresElegidos;
+// }
 
 
-const menu = (usuario) => {
-    let finalizado = false;
-    let producto = new Producto(0,[],0);
-    let compra =  new Compra([],false,0);
-    let error = 0;
+// const menu = (usuario) => {
+//     let finalizado = false;
+//     let producto = new Producto(0,[],0);
+//     let compra =  new Compra([],false,0);
+//     let error = 0;
     
-    while ((finalizado === false) && (compra.estado === false)){
+//     while ((finalizado === false) && (compra.estado === false)){
         
-        let op = parseInt(prompt
-            ("Ihelados🍦\n Por Favor Escoja el tamaño de Su helado: \n 1. 1/4kg $1000\n 2. 1/2kg  $2000\n 3. 1kg $4000\n 4. Pagar \n 5.Cancelar y Salir"));
-        switch (op) {
-            case 1:
+//         let op = parseInt(prompt
+//             ("Ihelados🍦\n Por Favor Escoja el tamaño de Su helado: \n 1. 1/4kg $1000\n 2. 1/2kg  $2000\n 3. 1kg $4000\n 4. Pagar \n 5.Cancelar y Salir"));
+//         switch (op) {
+//             case 1:
                 
-                compra.productos.push(new Producto(op,escogerSabores(3,1),1000));
-                alert(`Los sabores elegidos fueron: ${producto.sabores.join(',')}`);
-                break;
-            case 2: 
-                compra.productos.push(new Producto(op,escogerSabores(3,2),2000));
-                alert(`Los sabores elegidos fueron: ${producto.sabores.join(',')}`);
-                break;
-            case 3: 
-                compra.productos.push(new Producto(op,escogerSabores(3,3),4000));
-                alert(`Los sabores elegidos fueron: ${producto.sabores.join(',')}`);
-                break;            
-            case 4:
-                compra.calcularMonto();
+//                 compra.productos.push(new Producto(op,escogerSabores(3,1),1000));
+//                 alert(`Los sabores elegidos fueron: ${producto.sabores.join(',')}`);
+//                 break;
+//             case 2: 
+//                 compra.productos.push(new Producto(op,escogerSabores(3,2),2000));
+//                 alert(`Los sabores elegidos fueron: ${producto.sabores.join(',')}`);
+//                 break;
+//             case 3: 
+//                 compra.productos.push(new Producto(op,escogerSabores(3,3),4000));
+//                 alert(`Los sabores elegidos fueron: ${producto.sabores.join(',')}`);
+//                 break;            
+//             case 4:
+//                 compra.calcularMonto();
                 
-                alert("Usted debera Abonar $"+compra.monto+" Muchas Gracias por su compra!");
-                compra.estado=true;
-                break;
-            case 5:
-                alert("!Hasta luego,vuelva pronto¡");
-                finalizado = true;
-                break; 
-            default:
-                alert('No ingreso una opcion valida intentelo nuevamente');
-                if(error < 5){
-                  error = error +1;
-                  continue;
-                }
-                else{
-                    alert('Ha ingresado una opcion Erronea por quinta vez,Reinice la ventana de navegacion ');
-                    finalizado=true;
-                };
-                break;  
-        };
+//                 alert("Usted debera Abonar $"+compra.monto+" Muchas Gracias por su compra!");
+//                 compra.estado=true;
+//                 break;
+//             case 5:
+//                 alert("!Hasta luego,vuelva pronto¡");
+//                 finalizado = true;
+//                 break; 
+//             default:
+//                 alert('No ingreso una opcion valida intentelo nuevamente');
+//                 if(error < 5){
+//                   error = error +1;
+//                   continue;
+//                 }
+//                 else{
+//                     alert('Ha ingresado una opcion Erronea por quinta vez,Reinice la ventana de navegacion ');
+//                     finalizado=true;
+//                 };
+//                 break;  
+//         };
 
-    };
+//     };
 
-    if(compra.estado){
-       usuario.compras.push(compra); 
-    }
+//     if(compra.estado){
+//        usuario.compras.push(compra); 
+//     }
       
 
 
-};
+// };
